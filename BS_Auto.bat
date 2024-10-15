4593c8c4541a9347c7c80f9242c5591bd96e526b
DuyThanhLieu <duythanhlieu2001@gmail.com>
2024-10-15 10:30:12 +0700

    aa

---
+++ BS_Auto.bat
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Busai GPT
+
+# Uncomment the following lines to run additional tests:
+# npx playwright test BS_Cookies.spec.js --headed --reporter=line --workers=1
+# npx playwright test BS_Actions.spec.js --headed --reporter=line --workers=1
+# npx playwright test BS_Music_Actions.spec.js --reporter=line --workers=1
+
+# npx playwright test BS_Actions.spec.js
+
+echo "Bắt đầu thực hiện kiểm tra BS_Actions.spec.js"
+call npx playwright test BS_Actions.spec.js --reporter=html --output=./Results --workers=1
+echo "Kiểm tra BS_Actions.spec.js đã hoàn thành"
